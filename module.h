58683f68abc8508cfbc334a383cd04c7804e22a8
kaden <thomas.kaden4@outlook.com>
2016-12-14 15:11:29 -0800

    Change

--- module.h
+++ module.h
@@ -4,28 +4,45 @@
  *
  * Submodules may be added to the system 
  * to provide new functionality to the 
- * Karat system. Each module may use 
- * up to 2 of the 16 predifined io ports
+ * Karat system. Each module may use
+ * io ports to communicate with the cpu.
+ * code can use input to make decisions
  */
 
 #include<stdint.h>
+#include<wchar.h>
 
 #include"karat.h"
+#include"list.h"
 
-typedef void(*read_f)(void *, uint32_t);
+typedef void (*read_f)(void *, uint32_t);
+typedef struct { read_f cb; void *data; } read_cb;
+#define CALL_CB(x, ...) x.cb(x.data, ##__VA_ARGS__)
 
 struct mod {
-	addr_t address;
-	read_f on_read;
+	NODE(struct mod);
+	/* name of the module */
+	const wchar_t *name;
+	/* what to do when cpu writes to port */
+	read_cb on_read;
 };
 
 struct modlist {
+	size_t size;
+	struct mod *head;
+	struct mod *tail;
 };
 
-API void modules_init()
+/* initialize a module list with the maximum */
+API void modules_init(struct modlist *list);
 /* load a module from file */
-API void load_module(struct mod *m, const wchar_t *file);
-/* create a raw module from function (for builtin modules) */
-API void load_internal_module(struct mod *m, read_f sys_on_read);
-/* remove a module */
-API void unload_module(addr_t from_port);
+API struct mod *load_module(struct modlist *list, const wchar_t *file);
+/* create a raw module from callback (for builtin modules) */
+API struct mod *load_internal_module(	struct modlist *list, 	
+										read_cb update_cb, 
+										const wchar_t *name	);
+/* call the update method of a module with the given data */
+API void update_module(struct mod *m, uint32_t data);
+/* remove a module, if it does not exist, no effect */
+API void unload_module(struct modlist *list, struct mod *m);
+API void modules_destroy(struct modlist *list);
