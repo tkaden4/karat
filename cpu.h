f6e331a8a03e67c8a3087d0a8c2730d1e92174bd
kaden <thomas.kaden4@outlook.com>
2016-12-13 23:14:06 -0800

    Added cpu data

--- cpu.h
+++ cpu.h
@@ -12,33 +12,50 @@ typedef uint16_t addr_t;
 #define GENERAL_REGS 8	/* number of general registers */
 #define ADDRESS_REGS 8	/* number of address registers */
 
-#if GENERAL_REGS < 1
-#error
-#endif
-
-#if ADDRESS_REGS < 2
-#error
-#endif
-
 #define SP_INDEX (ADDRESS_REGS - 2)
 #define FP_INDEX (ADDRESS_REGS - 1)
 
-#define AREG(cpu, i) cpu.aregs[i]
-#define GREG(cput, i) cpu.gregs[i]
-
-#define SP(cpu) AREG(SP_INDEX)
-#define FP(cpu)	AREG(FP_INDEX) 
-
 enum {
 	NMI = 0,
-	IRQ,
-	MAX_INTERRUPTS
+	IRQ, 
+	INTERRUPTS
 };
 
-#define INTERRUPT(cpu, i) 
-
 struct cpu_state {
-	greg_t gregs[GENERAL_REGS];	/* general registers */
-	areg_t aregs[ADDRESS_REGS];	/* address registers */
-	addr_t interrupts[MAX_INTERRUPTS];		/* interrupts */
+	/* general registers */
+	union {
+		greg_t gregs[GENERAL_REGS];
+		struct {
+			greg_t g0;
+			greg_t g1;
+			greg_t g2;
+			greg_t g3;
+			greg_t g4;
+			greg_t g5;
+			greg_t g6;
+			greg_t g7;	
+		};
+	};
+	/* address registers */
+	union {
+		areg_t aregs[ADDRESS_REGS];
+		struct {
+			areg_t a0;
+			areg_t a1;
+			areg_t a2;
+			areg_t a3;
+			areg_t a4;
+			areg_t a5;
+			areg_t sp;	/* stack pointer */
+			areg_t fp;	/* frame pointer */
+		};
+	};
+	/* interrupts */
+	union {
+		addr_t interrupts[INTERRUPTS];
+		struct {
+			addr_t nmi;
+			addr_t irq;
+		};
+	};
 };
