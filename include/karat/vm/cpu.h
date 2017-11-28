3b182e9f36403152e71648573bda4a06bf2cfac0
tkaden4 <thomas.kaden4@outlook.com>
2017-11-27 18:17:46 -0800

    fix cpu.h

--- include/karat/vm/cpu.h
+++ include/karat/vm/cpu.h
@@ -5,6 +5,7 @@
 /* karat files */
 #include<karat/vm/kprog.h>
 #include<karat/ktypes.h>
+#include<karat/log.h>
 
 typedef uint32_t reg_t; /* general register datatype */
 
@@ -16,7 +17,7 @@ struct cpu {
     union {
         reg_t regs[GENERAL_REGS];
         struct {
-            reg_t __pad[GENERAL_REGS - 3];
+            reg_t __pad[GENERAL_REGS - 2];
             reg_t sp;
             reg_t pc;
         };
@@ -27,4 +28,12 @@ static inline void cpu_init(struct cpu *cpu, reg_t pc, reg_t sp)
 {
     cpu->sp = sp;
     cpu->pc = pc;
+}
+
+static void print_cpu_info(struct cpu *cpu)
+{
+    err_on(!cpu, "cpu is NULL");
+    for(size_t i = 0; i < GENERAL_REGS; ++i){
+        printf("r%lu: %u\n", i, cpu->regs[i]);
+    }
 }
